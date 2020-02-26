0965b9d84f30bf1636b2a88aaf59c4531f816057
bmulas1535 <bmulas1535@users.noreply.github.com>
2020-02-26 13:29:50 -0800

    base level architecture

---
+++ app.py
@@ -0,0 +1,35 @@
+# ========================================================================== #
+#                                                                            #
+#      @ AUTHOR : Brandon Mulas                                              #
+#      @   DATE : 20200226114738                                             #
+#      @  TITLE : demo-dash                                                  #
+#                                                                            #
+# ========================================================================== #
+import dash
+import dash-bootstrap-components as dbc
+import dash-core-components as dcc
+import dash-html-components as html
+
+
+""" 
+    External stylesheets is going to be linked to the bootstrap cdn
+    bootswatch template. Use this template to give yourself fast styling
+    options.
+"""
+ext_style = [dbc.themes.DARKLY]
+
+"""
+    Meta tags are meta
+"""
+meta_tags = [{'name':'viewport', 
+              'content':'width=device-width, 
+                         initial-scale=1'}]
+
+"""
+    Define app as dash.Dash object. Set external stylesheets and meta tags.
+"""
+app = dash.Dash(__name__,
+                external_stylesheets=ext_style, 
+                meta_tags=meta_tags)
+app.title = "My Awesome Title"
+server = app.server
---
+++ run.py
@@ -0,0 +1,64 @@
+import dash
+import dash-bootstrap-components as dbc
+import dash-core-components as dcc
+import dash-html-components as html
+from dash.dependencies import Input, Output
+
+# import app and server from application
+from app import app, server
+# import pages
+from pages import <pages>
+
+
+"""
+    Creating the base layout
+"""
+navbar = dbc.NavbarSimple(brand="My Awesome Brand", 
+                          brand_href='/', 
+                          children=[dbc.NavItem(
+                                            dcc.Link("Page 1", 
+                                            href="/page1", 
+                                            className="nav-link")), 
+                                    dbc.NavItem(
+                                            dcc.Link("Page 2", 
+                                            href="/page2", 
+                                            className="nav-link"))
+                                    ],
+                          sticky="top")
+
+footer = dbc.Container(dbc.Row(dbc.Col(html.P([
+    html.Span("I am awesome", className="mr-2"), 
+    html.A(html.I(className="fas fa-envelope-sqaure mr-1"),
+                  href="/"),
+    html.A(html.I(className="fab fa-github-square mr-1"),
+                  href="/"), 
+    html.A(html.I(className="fab fa-linkedin-square mr-1"),
+                  href="/"), 
+    html.A(html.I(className="fab fa-twitter-square mr-1"), 
+                  href="/")
+    ],
+    className="lead"))))
+
+# Establish layout
+app.layout = html.Div([dcc.Location(id='url', refresh=False),
+                       navbar,
+                       dbc.Container(id="page-content", className='mt-2'), 
+                       html.Hr(), 
+                       footer])
+
+# Do the displaying of the pages thing
+@app.callback(Output("page-content", "children"),
+             [Input("url", "pathname")])
+def display_page(pathname):
+    if pathname == "/":
+        return index.layout
+    elif pathname == "/page1":
+        return page1.layout
+    elif pathname == "/page2":
+        return page2.layout
+    else:
+        return dcc.Markdown("### Nope.")
+
+# Do the run thing
+if __name__ == "__main__":
+    app.run_server(debug=True)
